267c7ad4381ffc9d7569ebf31410632050e6baf9
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-09-30 10:13:24 +0100

    fix memory leak

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/service/modal.service';
 
 @Component({
@@ -14,5 +14,11 @@ export class AuthModalComponent implements OnInit {
   ngOnInit(): void {
     this.modal.register("auth")
   }
+
+  ngOnDestroy() {
+    this.modal.unregister("auth")
+  }
+
+
 
 }
