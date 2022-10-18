0d0dffd394a18f8f723017d5b875d76184cb76e2
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-10-18 15:57:53 +0100

    add form control&goupe to register

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class RegisterComponent {
+  /* After creating new instance we'll be able
+  to retrieve info related to the form and controle it*/
+  registgerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  })
 
 }
