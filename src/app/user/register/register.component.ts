9913512dd2ca20458ea179c845fe474d2bea0cd4
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-10-19 14:49:31 +0100

    refacotring inputs/reusiblity

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,23 +8,29 @@ import { FormGroup, FormControl, Validators, AbstractControl } from "@angular/fo
 })
 export class RegisterComponent {
 
-  nameInput: any = AbstractControl;
 
-  constructor() {
-    this.nameInput = this.registgerForm.controls["name"];
-  }
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  email = new FormControl('')
+  age = new FormControl('')
+  password = new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber = new FormControl('')
   /* After creating new instance we'll be able
   to retrieve info related to the form and controle it*/
   registgerForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    name: this.name,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+
   })
+
+
+  constructor() {
+  }
 
 }
