dbcaa46cf9d0465ba5d9d4f213400421c0d647c9
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-10-19 16:07:52 +0100

    add custom validation to password

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -11,9 +11,11 @@ export class RegisterComponent {
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)])
   email = new FormControl('', [Validators.required, Validators.email])
-  age = new FormControl('')
-  password = new FormControl('')
-  confirm_password = new FormControl('')
+  age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)])
+  password = new FormControl('',
+    [Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)])
+  confirm_password = new FormControl('', [Validators.required])
   phoneNumber = new FormControl('')
 
 
