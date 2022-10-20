2b71272c4c38db5cc855d7f5e88c146f91435d69
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-10-20 10:17:53 +0100

    disbling submit according to validations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      opacity: ['disabled'],
+      backgroundColor: ['disabled']
+    },
   },
   plugins: [],
 }
