a2f2f2cb64bd4ec324c3e341f1a3414b9bb2dc6e
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-11-09 15:42:35 +0100

    delete clip db

--- src/app/service/clip.service.ts
+++ src/app/service/clip.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { IClip } from '../models/clip.model';
@@ -12,7 +13,8 @@ export class ClipService {
   private clipsCollection: AngularFirestoreCollection<IClip>;
 
   constructor(private db: AngularFirestore,
-    private auth: AngularFireAuth) {
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage) {
     this.clipsCollection = db.collection("clips");
   }
 
@@ -47,5 +49,14 @@ export class ClipService {
     return this.clipsCollection.doc(id).update({
       title
     })
+  }
+
+
+  async deleteClip(clip: IClip) {
+    const clipRef = this.storage.ref(`clips/${clip.fileName}`)
+    await clipRef.delete()
+
+
+    await this.clipsCollection.doc(clip.docId).delete()
   }
 }
