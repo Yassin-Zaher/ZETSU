3b86a1de4ffde2a8b719e739e4b5e62b1bd989d5
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-11-18 19:51:32 +0100

    initialase infinit scrolling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
 
 }
